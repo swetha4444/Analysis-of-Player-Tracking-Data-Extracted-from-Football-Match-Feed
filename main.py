8abd0aaea7b337544d0a64f45bd858f0cfa8f342
Swetha Saseendran <68152189+swetha4444@users.noreply.github.com>
2022-03-07 13:34:00 +0530

    deepsort

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from modules.yolo import YOLO
 from modules.yolo import *
+from modules.deepsort import *
+from yolov5.utils.plots import plot_one_box
 
 import torch
 import os
@@ -11,6 +13,7 @@ cap = cv2.VideoCapture("input3.mp4")
 w = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
 h = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
 detector = YOLO("models/yolov5s.pt",0.5, 0.3)
+deep_sort = DEEPSORT("deep_sort_pytorch/configs/deep_sort.yaml")
 
 while(cap.isOpened()):
     bbox = []
@@ -18,7 +21,16 @@ while(cap.isOpened()):
     if ret:
         main_frame = frame.copy()
         yoloOutput = detector.detect(frame)
-        frame = draw_boxes(frame, yoloOutput, identities=None, offset=(0, 0))
+        
+        if yoloOutput:
+                deep_sort.detection_to_deepsort(yoloOutput, frame)
+
+                for i, obj in enumerate(yoloOutput):
+                    xyxy = [obj['bbox'][0][0], obj['bbox'][0][1], obj['bbox'][1][0], obj['bbox'][1][1]]
+                    x_center = (xyxy[0] + xyxy[2])/2 
+                    y_center = xyxy[3]
+        else:
+           deep_sort.deepsort.increment_ages()
     
         cv2.imshow('frame',frame)
         if cv2.waitKey(1) & ord('q') == 0xFF:
---
+++ modules/resources.py
@@ -0,0 +1,43 @@
+import cv2
+from sklearn.cluster import KMeans
+import numpy as np
+
+
+pallete = {'b': (0, 0, 128),
+        'g': (0, 128, 0),
+        'r': (255, 0, 0),
+        'c': (0, 192, 192),
+        'm': (192, 0, 192),
+        'y': (192, 192, 0),
+        'k': (0, 0, 0),
+        'w': (255, 255, 255)}
+
+color_for_labels = (2 ** 11 - 1, 2 ** 15 - 1, 2 ** 20 - 1)
+
+
+def compute_color_for_labels(label):
+    """
+    Simple function that adds fixed color depending on the class
+    """
+    color = [int((p * (label ** 2 - label + 1)) % 255) for p in color_for_labels]
+    return tuple(color)
+
+
+def draw_boxes(img, bbox, identities=None, offset=(0, 0)):
+    for i, box in enumerate(bbox):
+        x1, y1, x2, y2 = [int(i) for i in box]
+        x1 += offset[0]
+        x2 += offset[0]
+        y1 += offset[1]
+        y2 += offset[1]
+        # box text and bar
+        id = int(identities[i]) if identities is not None else 0
+        color = compute_color_for_labels(id)
+        label = '{}{:d}'.format("", id)
+        t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 2, 2)[0]
+        cv2.rectangle(img, (x1, y1), (x2, y2), color, 3)
+        cv2.rectangle(
+            img, (x1, y1), (x1 + t_size[0] + 3, y1 + t_size[1] + 4), color, -1)
+        cv2.putText(img, label, (x1, y1 +
+                                 t_size[1] + 4), cv2.FONT_HERSHEY_PLAIN, 2, [255, 255, 255], 2)
+    return img
