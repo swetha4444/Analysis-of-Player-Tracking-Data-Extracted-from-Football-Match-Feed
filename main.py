9f94a087b3348d95a7301eeb1d1bb4919d7fc6a8
Swetha Saseendran <68152189+swetha4444@users.noreply.github.com>
2022-04-19 00:03:48 +0530

    .

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import cv2
 import numpy as np
 from modules.resources import *
 from modules.jerseycolor import *
+import matplotlib.pyplot as plt
+
+from mplsoccer import Pitch, VerticalPitch
 
 
 frame_num = 0
@@ -17,7 +20,7 @@ detector = YOLO("models/yolov5s.pt",0.5, 0.3)
 deep_sort = DEEPSORT("deep_sort_pytorch/configs/deep_sort.yaml")
 
 gt_img = cv2.imread('black.jpg')
-gt_img= cv2.cvtColor(gt_img, cv2.COLOR_BGR2RGB)
+gt_h, gt_w, _ = gt_img.shape
 
 frame_width = int(cap.get(3))
 frame_height = int(cap.get(4))
@@ -27,11 +30,16 @@ result = cv2.VideoWriter('output.mp4',
                          20, size)
 
 while(cap.isOpened()):
+    pitch = Pitch(pitch_type='tracab',  # example plotting a tracab pitch
+                    pitch_length=280, pitch_width=280,
+                    axis=True, label=True)  # showing axis labels is optional
+    fig, ax = pitch.draw()
+    x = []
+    y = []
+    
     bbox = []
     ret,frame = cap.read()
     bg_img = gt_img.copy()
-    frame = cv2.resize(frame,(320, 320))
-    bg_img = cv2.resize(bg_img,(320, 320))
     
     if ret:
         main_frame = frame.copy() 
@@ -42,11 +50,12 @@ while(cap.isOpened()):
             M,gt_h, gt_w = getHomogrpahyMatrix('black.jpg',frame)
             #M = np.linalg.inv(M)
             visualise_homography(frame,'black.jpg',M)
-            
+        
         
         if yoloOutput:
                 deep_sort.detection_to_deepsort(yoloOutput, frame)
 
+                gt_img = cv2.resize(gt_img,(280,280))
                 for i, obj in enumerate(yoloOutput):
                     xyxy = [obj['bbox'][0][0], obj['bbox'][0][1], obj['bbox'][1][0], obj['bbox'][1][1]]
                     x_center = (xyxy[0] + xyxy[2])/2 
@@ -57,6 +66,8 @@ while(cap.isOpened()):
                         except:
                           pass
                         coords = transform_matrix(M, (x_center, y_center), (h, w), (gt_h, gt_w))
+                        x.append(coords[0])
+                        y.append(coords[1])
                         cv2.circle(bg_img, coords, 5, (255,0,0), -1)
                         #cv2.putText(bg_img, str(i), coords, cv2.FONT_HERSHEY_PLAIN, 2, [255, 255, 255], 1)
                     
@@ -73,7 +84,10 @@ while(cap.isOpened()):
         cv2.imshow('frame',bg_img)
         #frame = cv2.resize(frame, size)
         #result.write(frame)
-        
+        df_shots = pd.DataFrame(list(zip(x,y)),columns=['x','y'])
+        sc = pitch.scatter(df_shots.x, df_shots.y,ax=ax)
+        fig.savefig("pitch_viz.png")
+        print(df_shots)
         key = cv2.waitKey(1) & 0xFF
         if key == ord("q"):
             break
--- modules/resources.py
+++ modules/resources.py
@@ -55,4 +55,5 @@ def transform_matrix(matrix, p, vid_shape, gt_shape):
     p = (p[0]/vid_shape[1], p[1]/vid_shape[0])
     px = (matrix[0][0]*p[0] + matrix[0][1]*p[1] + matrix[0][2]) / ((matrix[2][0]*p[0] + matrix[2][1]*p[1] + matrix[2][2]))
     py = (matrix[1][0]*p[0] + matrix[1][1]*p[1] + matrix[1][2]) / ((matrix[2][0]*p[0] + matrix[2][1]*p[1] + matrix[2][2]))
-    return (int(px*gt_shape[1]) , int(py*gt_shape[0]))
+    p_after = (int(px*gt_shape[1]) , int(py*gt_shape[0]))
+    return p_after
