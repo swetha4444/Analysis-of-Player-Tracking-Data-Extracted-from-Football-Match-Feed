2940571b6cef329f029fd5e0667c33ed34411df6
swetha4444 <swethasaseendran4@gmail.com>
2022-04-26 01:57:38 +0530

    streamlit app

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from modules.helper import *
 
 inputPath = "./static/input3.mp4"
-outputPath = "./static/input3.mp4"
+outputPath = "./static/output.mp4"
 
 playerTrackingData,ballTrackingData = pipeline(inputPath,outputPath)
 formatData(playerTrackingData,ballTrackingData)
