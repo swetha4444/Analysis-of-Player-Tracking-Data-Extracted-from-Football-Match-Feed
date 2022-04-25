fdf489d4060f33a3ae5e730198e2c8b795a975cc
swetha4444 <swethasaseendran4@gmail.com>
2022-04-26 01:56:12 +0530

    test

--- test/test.py
+++ test/test.py
@@ -1,12 +1,14 @@
-import cv2
-import pandas as plt
+def model_test():
+    pass
 
-cap = cv2.VideoCapture("input3.mp4")
-gt_img = cv2.imread('world_cup_template.png')
-gt_img= cv2.cvtColor(gt_img, cv2.COLOR_BGR2RGB)
-gt_img = cv2.resize(gt_img, (1280,720))/255.
-bg_img = gt_img.copy()
+def tracking_unit_test():
+    pass
 
-cv2.circle(bg_img, (366, 825), 5, (0,0,255), -1)
-while(cap.isOpened()):
-    cv2.imshow('frame',bg_img)
+def jeresey_unit_test():
+    pass
+
+def homography_unit_test():
+    pass
+
+def intergration_test():
+    pass
